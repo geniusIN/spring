d9732a463cbef9f7f2e85e39e4b81fee5612b3aa
geniusIN <rlaghddls9309@gmail.com>
2025-07-01 17:52:38 +0900

    7월1일 spring 수업

--- sp02/src/main/java/com/yedam/app/Sp02Application.java
+++ sp02/src/main/java/com/yedam/app/Sp02Application.java
@@ -1,13 +1,23 @@
 package com.yedam.app;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication 
+//이거 풀어주면 아래 3개 나옴.
+@SpringBootConfiguration
+@EnableAutoConfiguration // auto-config
+@ComponentScan // spring
+
+@MapperScan(basePackages = "com.yedam.app.**.mapper") //mybatis
 public class Sp02Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sp02Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sp02Application.class, args);
+    }
 
 }
